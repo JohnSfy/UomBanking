6cca752221302997b7b09ad92e8ee4ef5172d27a
UOMSystemX <dimsparagis@gmail.com>
2023-06-06 20:54:04 +0300

    Added functionality for Transfer, LogOut

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -66,11 +66,6 @@ public class MainFrame extends JFrame {
 //      Setting up createCard button
         createCardButton.setBounds(370, 710, 250, 30);
 
-
-
-
-
-
         createCardButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
